3ad3446e3ae733f3c621f5c0cb93abdd5a4c504b
Pavel <pavsmel@hotmail.com>
2025-04-24 13:27:48 +0300

    add prettier; join hardhat configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,52 @@
-import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-contract-sizer";
-import * as defaultConfig from "./hardhat.common";
-import { config as dotEnvConfig } from "dotenv";
-import "./tasks/deploy";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-contract-sizer';
+import { config as dotEnvConfig } from 'dotenv';
+import '@nomicfoundation/hardhat-foundry';
+// import "./tasks/deploy";
 
 dotEnvConfig();
 
-const config = {
-  ...defaultConfig.default,
-  networks: {
-    ethereum: {
-      url: process.env.ETH_RPC_URL,
+const config: HardhatUserConfig & { contractSizer: any } = {
+    solidity: {
+        compilers: [
+            {
+                version: '0.8.28',
+                settings: {
+                    viaIR: true,
+                    optimizer: {
+                        enabled: true,
+                        runs: 1000000,
+                    },
+                },
+            },
+        ],
     },
-    sepolia: {
-      url: process.env.SEPOLIA_RPC_URL,
+    etherscan: {
+        apiKey: process.env.API_KEY,
     },
-  },
+    mocha: {
+        timeout: 2_000_000,
+    },
+    contractSizer: {
+        alphaSort: true,
+        disambiguatePaths: false,
+        runOnCompile: true,
+        strict: false,
+        only: ['Levva', 'Vesting', 'Staking', 'TokenMinter'],
+        except: ['Mock', 'Test'],
+    },
+    sourcify: {
+        enabled: false,
+    },
+    networks: {
+        ethereum: {
+            url: process.env.ETH_RPC_URL,
+        },
+        sepolia: {
+            url: process.env.SEPOLIA_RPC_URL,
+        },
+    },
 };
 
 export default config;
--- test/LevvaVault.t.sol
+++ test/LevvaVault.t.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-import {Test} from "lib/forge-std/src/Test.sol";
-import {Vm} from "lib/forge-std/src/Vm.sol";
-import {console} from "lib/forge-std/src/console.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Test} from 'lib/forge-std/src/Test.sol';
+import {Vm} from 'lib/forge-std/src/Vm.sol';
+import {console} from 'lib/forge-std/src/console.sol';
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 contract LevvaVaultTest is Test {
     function setUp() public {}
 
     function test() public {
-        console.log("Test");
+        console.log('Test');
     }
 }
