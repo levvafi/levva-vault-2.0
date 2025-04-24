1f121643404255a47b71a1bb28f4327e5c9c86d0
Pavel <pavsmel@hotmail.com>
2025-04-24 14:10:27 +0300

    format .sol files with forge; remove redundant deps

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,45 +8,45 @@ import '@nomicfoundation/hardhat-foundry';
 dotEnvConfig();
 
 const config: HardhatUserConfig & { contractSizer: any } = {
-    solidity: {
-        compilers: [
-            {
-                version: '0.8.28',
-                settings: {
-                    viaIR: true,
-                    optimizer: {
-                        enabled: true,
-                        runs: 1000000,
-                    },
-                },
-            },
-        ],
-    },
-    etherscan: {
-        apiKey: process.env.API_KEY,
-    },
-    mocha: {
-        timeout: 2_000_000,
-    },
-    contractSizer: {
-        alphaSort: true,
-        disambiguatePaths: false,
-        runOnCompile: true,
-        strict: false,
-        only: ['Levva', 'Vesting', 'Staking', 'TokenMinter'],
-        except: ['Mock', 'Test'],
-    },
-    sourcify: {
-        enabled: false,
-    },
-    networks: {
-        ethereum: {
-            url: process.env.ETH_RPC_URL,
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.28',
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 1000000,
+          },
         },
-        sepolia: {
-            url: process.env.SEPOLIA_RPC_URL,
-        },
+      },
+    ],
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  },
+  mocha: {
+    timeout: 2_000_000,
+  },
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: false,
+    only: ['Levva', 'Vesting', 'Staking', 'TokenMinter'],
+    except: ['Mock', 'Test'],
+  },
+  sourcify: {
+    enabled: false,
+  },
+  networks: {
+    ethereum: {
+      url: process.env.ETH_RPC_URL,
     },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+    },
+  },
 };
 
 export default config;
