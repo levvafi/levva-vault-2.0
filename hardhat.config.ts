5895d7c47a2e986f37d45e7b5774845bd5130a5a
optifat <pavsmel@mail.ru>
2025-04-24 14:13:58 +0300

    complete repo setup (#1)
    * add file formatting: forge for sol and prettier for ts
    
    * join hardhat configs
    
    * remove redundant deps

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,44 @@
-import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-contract-sizer";
-import * as defaultConfig from "./hardhat.common";
-import { config as dotEnvConfig } from "dotenv";
-import "./tasks/deploy";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-contract-sizer';
+import { config as dotEnvConfig } from 'dotenv';
+import '@nomicfoundation/hardhat-foundry';
+// import "./tasks/deploy";
 
 dotEnvConfig();
 
-const config = {
-  ...defaultConfig.default,
+const config: HardhatUserConfig & { contractSizer: any } = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.28',
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 1000000,
+          },
+        },
+      },
+    ],
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  },
+  mocha: {
+    timeout: 2_000_000,
+  },
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: false,
+    only: ['Levva', 'Vesting', 'Staking', 'TokenMinter'],
+    except: ['Mock', 'Test'],
+  },
+  sourcify: {
+    enabled: false,
+  },
   networks: {
     ethereum: {
       url: process.env.ETH_RPC_URL,
