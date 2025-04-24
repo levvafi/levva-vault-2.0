669544a89671c0f49a07d5f6ab4b3e9fce9fec50
Pavel <pavsmel@hotmail.com>
2025-04-24 15:33:07 +0300

    basic tests setup

--- test/LevvaVault.t.sol
+++ test/LevvaVault.t.sol
@@ -5,11 +5,38 @@ import {Test} from "lib/forge-std/src/Test.sol";
 import {Vm} from "lib/forge-std/src/Vm.sol";
 import {console} from "lib/forge-std/src/console.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+import {LevvaVault} from "../contracts/LevvaVault.sol";
+import {MintableERC20} from "./MintableERC20.t.sol";
 
 contract LevvaVaultTest is Test {
-    function setUp() public {}
+    LevvaVault public levvaVaultImplementation;
+    ERC1967Proxy public levvaVaultProxy;
+    LevvaVault public levvaVault;
 
-    function test() public {
-        console.log("Test");
+    MintableERC20 public asset;
+
+    function setUp() public {
+        asset = new MintableERC20("USDTest", "USDTest", 6);
+
+        levvaVaultImplementation = new LevvaVault();
+        bytes memory data = abi.encodeWithSelector(LevvaVault.initialize.selector, IERC20(asset));
+        levvaVaultProxy = new ERC1967Proxy(address(levvaVaultImplementation), data);
+        levvaVault = LevvaVault(address(levvaVaultProxy));
+    }
+
+    function testInitialize() public view {
+        assertEq(address(levvaVault.asset()), address(asset));
+        assertEq(levvaVault.owner(), address(this));
+    }
+
+    function testTotalAssets() public {
+        uint256 depositAmount = 10 ** 12;
+        asset.mint(address(this), depositAmount);
+        asset.approve(address(levvaVault), depositAmount);
+        levvaVault.deposit(depositAmount, address(this));
+
+        assertEq(levvaVault.totalAssets(), depositAmount);
     }
 }
---
+++ test/MintableERC20.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
+
+/// @dev Mintable ERC20 token.
+contract MintableERC20 is ERC20 {
+    uint8 private immutable _decimals;
+
+    constructor(string memory name, string memory symbol, uint8 decimals_) ERC20(name, symbol) {
+        _decimals = decimals_;
+    }
+
+    function mint(address account, uint256 amount) external {
+        _mint(account, amount);
+    }
+
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
+}
