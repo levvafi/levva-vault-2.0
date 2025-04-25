cfbe9fc37986f3b8432c20fe061fe621ab1da48a
rudewalt <rudewalt@gmail.com>
2025-04-25 13:07:59 +0300

    feat(adapter): add interfaces for adapters and adaptercallback (#3)
    * feat(adapter): add interfaces for adapters and adaptercallback
    
    * code review, add additional argument in callback
    
    ---------
    
    Co-authored-by: Ivan Sanin <ivan.sanin@eqlab.online>

---
+++ contracts/interfaces/IAdapterCallback.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+interface IAdapterCallback {
+    /// @notice Callback should transfer tokens to the receiver (adapter or external router depends on the adapter implementation)
+    /// @dev Levva vault should implement this function
+    /// @param token Address of token
+    /// @param receiver Address of token receiver
+    /// @param amount Amount of tokens to transfer
+    /// @param data Additional callback data
+    function adapterCallback(address receiver, address token, uint256 amount, bytes calldata data) external;
+}
