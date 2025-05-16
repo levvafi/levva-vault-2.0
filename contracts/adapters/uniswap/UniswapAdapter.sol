bcdcede09c5cd6468b55924fe5c656b166eb520f
Pavel <pavsmel@hotmail.com>
2025-05-16 17:38:20 +0300

    remove redundant IERC165 inheritance

--- contracts/adapters/uniswap/UniswapAdapter.sol
+++ contracts/adapters/uniswap/UniswapAdapter.sol
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.28;
 
-import {IERC165} from "@openzeppelin/contracts/interfaces/IERC165.sol";
-
 import {IAdapter} from "../../interfaces/IAdapter.sol";
 import {AbstractUniswapV3Adapter} from "./AbstractUniswapV3Adapter.sol";
 import {AbstractUniswapV4Adapter} from "./AbstractUniswapV4Adapter.sol";
 
-contract UniswapAdapter is IERC165, AbstractUniswapV3Adapter, AbstractUniswapV4Adapter {
+contract UniswapAdapter is AbstractUniswapV3Adapter, AbstractUniswapV4Adapter {
     bytes4 public constant getAdapterId = bytes4(keccak256("UniswapAdapter"));
 
     constructor(address v3Router, address universalRouter, address _permit2)
