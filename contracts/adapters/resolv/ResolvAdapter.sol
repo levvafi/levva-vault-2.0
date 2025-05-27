f3423c9f2be5275314c644bd793e6232da76f9ac
optifat <pavsmel@hotmail.com>
2025-05-27 17:32:56 +0300

    Feature/Resolv adapter (#18)
    * implement Resolv adapter

---
+++ contracts/adapters/resolv/ResolvAdapter.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.28;
+
+import {ERC4626AdapterBase} from "../ERC4626AdapterBase.sol";
+
+contract ResolvAdapter is ERC4626AdapterBase {
+    bytes4 public constant getAdapterId = bytes4(keccak256("ResolvAdapter"));
+
+    constructor(address _wstUSR) ERC4626AdapterBase(_wstUSR) {}
+
+    function USR() public view returns (address) {
+        return _asset;
+    }
+
+    function wstUSR() public view returns (address) {
+        return _vault;
+    }
+}
