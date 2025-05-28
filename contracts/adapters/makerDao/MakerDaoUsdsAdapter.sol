a2ac4512db46dad9ea92914f75e7dabc59091884
Pavel <pavsmel@hotmail.com>
2025-05-28 12:09:54 +0300

    implement MakerDao DAI and USDS adapters

---
+++ contracts/adapters/makerDao/MakerDaoUsdsAdapter.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.28;
+
+import {ERC4626AdapterBase} from "../ERC4626AdapterBase.sol";
+
+contract MakerDaoUsdsAdapter is ERC4626AdapterBase {
+    bytes4 public constant getAdapterId = bytes4(keccak256("MakerDaoUsdsAdapter"));
+
+    constructor(address _sUSDS) ERC4626AdapterBase(_sUSDS) {}
+
+    function USDS() public view returns (address) {
+        return _asset;
+    }
+
+    function sUSDS() public view returns (address) {
+        return _vault;
+    }
+}
